081327a4c6cb4caa197980d728de1ab5534be46c
M. Riedaa Gamieldien <61379972+gmlrie001@users.noreply.github.com>
2021-06-29 13:04:39 +0200

    Update index.blade.php

--- resources/views/templates/information_pages/index.blade.php
+++ resources/views/templates/information_pages/index.blade.php
@@ -10,7 +10,7 @@
     <div class="container">
       <div class="row">
           <div class="col-12 col-md-10 mx-auto sectionTitle py-lg-0 py-3">
-              <h2 class="text-left mb-0">{{ $page->title }}</h2>
+              <h2 class="text-left">{{ $page->title }}</h2>
           </div>
       </div>
     </div>
