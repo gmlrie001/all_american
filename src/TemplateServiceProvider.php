bc91c6234222056d3134d0acdbd4357c5d2005da
M. Riedaa Gamieldien <61379972+gmlrie001@users.noreply.github.com>
2021-07-13 11:33:56 +0200

    Separated publishable with their own tag name.

--- src/TemplateServiceProvider.php
+++ src/TemplateServiceProvider.php
@@ -19,21 +19,21 @@ class TemplateServiceProvider extends ServiceProvider
   {
     $this->publishes([
       dirname( __DIR__ ) . DIRECTORY_SEPARATOR . 'config/all_american.php' => config_path( 'all_american.php' ),
-    ], 'all_american' );
+    ], 'all_american_config' );
 
     $this->publishes([
       dirname( __DIR__ ) . DIRECTORY_SEPARATOR . 'public/assets' => public_path( 'assets' ),
-    ], 'all_american' );
+    ], 'all_american_assets' );
 
     if ( in_array( env( 'APP_ENV' ), ['local', 'development', 'dev'] ) ) {
       $this->publishes([
         dirname( __DIR__ ) . DIRECTORY_SEPARATOR . 'public/assets' => base_path( 'public/assets' ),
-      ], 'all_american_public_assets' );
+      ], 'all_american_assets_to_local_public_assets' );
     }
 
     $this->publishes([
       dirname( __DIR__ ) . DIRECTORY_SEPARATOR . 'resources/views/templates' => resource_path( 'views/templates' ),
-    ], 'all_american' );
+    ], 'all_american_templates' );
   }
 
   /**
