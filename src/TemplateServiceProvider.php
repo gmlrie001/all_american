491375bdcb28dfff6af3aa6be731be959cb8fbeb
gmlrie001 <dev2@monzamedia.com>
2021-07-16 14:28:07 +0200

    Working config...

--- src/TemplateServiceProvider.php
+++ src/TemplateServiceProvider.php
@@ -4,10 +4,12 @@ namespace Templates\AllAmerican;
 
 use Illuminate\Support\ServiceProvider;
 
+
 class TemplateServiceProvider extends ServiceProvider
 {
 
   // protected $defer = false;
+
 
   /**
    * Bootstrap the application services.
@@ -18,22 +20,21 @@ class TemplateServiceProvider extends ServiceProvider
   public function boot()
   {
     $this->publishes([
-      dirname( __DIR__ ) . DIRECTORY_SEPARATOR . 'config/all_american.php' => config_path( 'all_american.php' ),
-    ], 'all_american_config' );
+      dirname( __DIR__ ) .DIRECTORY_SEPARATOR. 'config/all_american.php' => config_path( 'all_american.php' ),
+    ], 'all_american_config');
 
     $this->publishes([
-      dirname( __DIR__ ) . DIRECTORY_SEPARATOR . 'public/assets' => public_path( 'assets' ),
-    ], 'all_american_assets' );
+      dirname( __DIR__ ) .DIRECTORY_SEPARATOR. 'public/assets' => config( 'all_american.template_destination' ),
+    ], 'all_american_assets');
 
     if ( in_array( env( 'APP_ENV' ), ['local', 'development', 'dev'] ) ) {
       $this->publishes([
-        dirname( __DIR__ ) . DIRECTORY_SEPARATOR . 'public/assets' => base_path( 'public/assets' ),
-      ], 'all_american_assets_to_local_public_assets' );
+        dirname( __DIR__ ) .DIRECTORY_SEPARATOR. 'public/assets' => base_path( 'public/assets' ),
+      ], 'all_american_assets_to_local_public_assets');
     }
 
-    $this->publishes([
-      dirname( __DIR__ ) . DIRECTORY_SEPARATOR . 'resources/views/templates' => resource_path( 'views/templates' ),
-    ], 'all_american_templates' );
+    $this->publishes([config( 'all_american.template_source' ) => config( 'all_american.template_destination' ),
+    ], config( 'all_american.template_tagname' ));
   }
 
   /**
@@ -42,7 +43,7 @@ class TemplateServiceProvider extends ServiceProvider
   public function register()
   {
     // Automatically apply the package configuration
-    $this->mergeConfigFrom( dirname( __DIR__ ) . DIRECTORY_SEPARATOR . 'config/all_american.php', 'all_american') ;
+    $this->mergeConfigFrom( dirname( __DIR__ ) .DIRECTORY_SEPARATOR. 'config/all_american.php', 'all_american' );
   }
 
   /**
