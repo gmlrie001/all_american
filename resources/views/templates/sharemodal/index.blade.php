c65cae719cd0e2e3b0d17a62343c4f8bf6a0f8a0
M. Riedaa Gamieldien <61379972+gmlrie001@users.noreply.github.com>
2021-08-03 14:37:00 +0200

    Update index.blade.php

--- resources/views/templates/sharemodal/index.blade.php
+++ resources/views/templates/sharemodal/index.blade.php
@@ -1,14 +1,11 @@
 <div class="share-overlay"></div>
 <div class="share-modal">
+
     <a class="close-share" href="javascript:void(0);">
-        <i class="fa fa-times" style="font-size:1.618rem;line-height:1.5;font-weight:bold;color:#000;"></i>
+        <i class="fa fa-times"></i>
     </a>
 
     <h1>Share {{$site_settings->site_name}}</h1>
-    
-    {{-- @if( NULL != $item_sharing && class_exists( 'Sharing' ) )
-        {!! Sharing::render( $item_sharing ) !!}
-    @endif --}}
     <div class="col-12 float-left text-center">
         <a rel="noopener noreferrer" target="_blank" href="http://www.facebook.com/sharer.php?s=100&p[url]={{Request::url('/')}}&p[title]={{$page->title}}&p[summary]={{strip_tags($page->description)}}" title="Share on Facebook" class="fab fa-facebook-f"></a>
         <a rel="noopener noreferrer" target="_blank" href="https://twitter.com/intent/tweet?url={{Request::url('/')}}&text={{strip_tags($page->description)}}&via={{$site_settings->site_name}}" title="Tweet on Twitter" class="fab fa-twitter"></a>
@@ -19,18 +16,19 @@
         <a rel="noopener noreferrer" target="_blank" href="http://www.linkedin.com/shareArticle?mini=true&url={{Request::url('/')}}" title="Share on LinkedIn" class="fab fa-linkedin"></a>
         <a rel="noopener noreferrer" target="_blank" href="whatsapp://send?text={{Request::url('/')}}" data-action="share/whatsapp/share" title="Share via Whatsapp" class="fab fa-whatsapp"></a>
         <a rel="noopener noreferrer" target="_blank" href="mailto:?Subject={{$site_settings->site_name}}&Body=Hi, Thought you might like this. {{Request::url('/')}}" title="Share via Email" class="fas fa-envelope"></a>
-    {{-- 
-      @forelse( $item_sharing as $platform => $share )
-        <a rel="noopener noreferrer" target="_blank" href="{{ $share[0] }}" title="Share on {{ $platform }}" class="{{ $share[1] }}"></a>
-      @empty
-      @endforelse --}}
+        {{-- 
+          @forelse( $item_sharing as $platform => $share )
+            <a rel="noopener noreferrer" target="_blank" href="{{ $share[0] }}" title="Share on {{ $platform }}" class="{{ $share[1] }}"></a>
+          @empty
+          @endforelse
+        --}}
     </div>
     
     <h1>Follow {{$site_settings->site_name}}</h1>
-    
     <div class="col-12 float-left text-center">
         @foreach($socials as $social)
             <a rel="noopener noreferrer" style="background-color:{{$social->social_media_icon_bg_colour}};color:{{$social->social_media_icon_colour}};" target="_blank" href="{{$social->social_media_url}}" class="fab {{$social->social_media_icon}}" title="{{$social->title}}"></a>
         @endforeach
     </div>
+
 </div>
